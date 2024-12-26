fcb633e2d41ec7e3112312231c89ff9ab5a75ab8
Muhammadqodir <alekscraft16@gmail.com>
2024-12-26 17:45:26 +0300

    lug`at

---
+++ python.py
@@ -0,0 +1,33 @@
+a = input("Qanday so`zning tarjimasi kerak: ")
+if a == 'Apple':
+    print("Olma")
+elif a == "Banana":
+    print("Banan")  
+elif a == "Morning":
+    print("Ertalab")  
+elif a == "Afternoon":
+    print("Peshin")  
+elif a == "Evening":
+    print("Kechki payt")  
+elif a == "Night":
+    print("Tun")  
+elif a == "Today":
+    print("Bugun")  
+elif a == "Tomorrow":
+    print("Ertaga")  
+elif a == "Tomorrow":
+    print("Kecha")  
+elif a == "Week":
+    print("Hafta")  
+elif a == "Month":
+    print("Oy")  
+elif a == "Year":
+    print("Oy")  
+elif a == "Remote control":
+    print("Pult")  
+elif a == "Air conditioner":
+    print("Conditsioner")  
+elif a == "Garden":
+    print("Bog`")  
+elif a == "Paper":
+    print("Varoq")  
